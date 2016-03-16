b6009a518589a19acd54d53ed840c81027047506
Jack Abernethy <jabber26@gmail.com>
2016-03-16 13:28:23 -0500

    Add react folder and boggle model

--- web/boggle.js
+++ web/boggle.js
@@ -8,7 +8,7 @@ Boggle.prototype.chooseRandom = function(value, index, array) {
 }
 
 Boggle.prototype.shake = function() {
-  BOGGLE_DICE.forEach(this.chooseRandom.bind(this));
+  BOGGLE_DICE.forEach(this.chooseRandom, this);
 }
 
 Boggle.prototype.include = function(word) {
@@ -16,8 +16,10 @@ Boggle.prototype.include = function(word) {
   var x = 0;
   while(y < 4) {
     while(x < 4) {
-      if(find(word.toUpperCase(), y++, x++, {})) return true;
+      if(this.find(word.toUpperCase(), y, x++, {})) return true;
     }
+    x = 0;
+    y++;
   }
   return false;
 }
@@ -25,28 +27,34 @@ Boggle.prototype.include = function(word) {
 Boggle.prototype.getNeighbors = function(x, y) {
   var neighbors = [];
   var i = -1;
-  var j = -1;
   while(i <= 1) {
+    var j = -1;
     while(j <= 1) {
-      var row = x + i++;
+      var row = x + i;
       var col = y + j++;
       var inbounds = row < 4 && row > -1 && col < 4 && col > -1;
-      if(inbounds) neighbors.push({row, col});
+      if(inbounds && !(row == y && col == x)) neighbors.push({row, col});
     }
+    i++;
   }
+  return neighbors;
 }
 
 Boggle.prototype.find = function(word, row, col, visited, wordIndex) {
   if(!wordIndex) wordIndex = 0;
-  if(!visited[row, col]) return false;
+  if(visited[[row, col]]) return false;
   var die = this.board[row][col];
   var letter = word[wordIndex];
   
-  if(wordIndex === word.length - 1) return word[wordIndex] === die;
+  if(wordIndex === word.length - 1) return letter === die;
 
-  if(die == letter) {
-    visited[row, col] = true;
-    var neighbors = getNeighbors(row, col);
+  if(die === letter) {
+    visited[[row, col]] = true;
+    var neighbors = this.getNeighbors(row, col);
+    var pass = neighbors.some(function(val, index){ 
+      return this.find(word, val.row, val.col, visited, wordIndex + 1)
+    });
+    if(pass) return true;
     visited[row, col] = false;
   }
 
