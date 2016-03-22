5330efd5806e0d69df7809af639c016502c30735
Jack Abernethy <jabber26@gmail.com>
2016-03-22 13:02:07 -0500

    Fix prototyping in cell

--- web/boggleCell.js
+++ web/boggleCell.js
@@ -5,46 +5,44 @@ function Cell(row, col, value, board) {
   this.board = board
 }
 
-Cell.prototype = {
-  find: function(word, visited=new Set(), wordIndex=0) {
-    if(visited.has(this)) return null;
-    word = word.toUpperCase();
-    var die = this.value;
-    var letter = word[wordIndex];
-    
-    if(wordIndex === word.length - 1) {
-      if(die !== letter) return null;
-      visited.add(this);
-      return visited;
-    }
+Cell.prototype.find = function(word, visited=new Set(), wordIndex=0) {
+  if(visited.has(this)) return null;
+  word = word.toUpperCase();
+  var die = this.value;
+  var letter = word[wordIndex];
+  
+  if(wordIndex === word.length - 1) {
+    if(die !== letter) return null;
+    visited.add(this);
+    return visited;
+  }
 
-    if(die === letter) {
-      visited.add(this);
-      var neighbors = this.getNeighbors(this.row, this.col);
-      var pass = neighbors.some(function(cell, index) { 
-        return cell.find(word, visited, wordIndex + 1)
-      }, this);
-      if(pass) return visited;
-    }
-    visited.delete(this);
-    return null;
-  },
+  if(die === letter) {
+    visited.add(this);
+    var neighbors = this.getNeighbors(this.row, this.col);
+    var pass = neighbors.some(function(cell, index) { 
+      return cell.find(word, visited, wordIndex + 1)
+    }, this);
+    if(pass) return visited;
+  }
+  visited.delete(this);
+  return null;
+};
 
-  getNeighbors: function() {
-    var neighbors = [];
-    var i = -1;
-    while(i <= 1) {
-      var j = -1;
-      var row = this.row + i;
-      while(j <= 1) {
-        var col = this.col + j++;
-        var neighbor = this.board.cell(row, col);
-        if(neighbor) neighbors.push(neighbor);
-      }
-      i++;
+Cell.prototype.getNeighbors = function() {
+  var neighbors = [];
+  var i = -1;
+  while(i <= 1) {
+    var j = -1;
+    var row = this.row + i;
+    while(j <= 1) {
+      var col = this.col + j++;
+      var neighbor = this.board.cell(row, col);
+      if(neighbor) neighbors.push(neighbor);
     }
-    return neighbors;
-  },
-}
+    i++;
+  }
+  return neighbors;
+};
 
 module.exports = Cell;
